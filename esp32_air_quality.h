a101b0b1fadb47b5275c7b6ce61898d3c06ea750
Lepidos <51986094+Lepidos@users.noreply.github.com>
2024-09-06 19:14:25 +0100

    Update esp32_air_quality.h
    fix comments

--- esp32_air_quality.h
+++ esp32_air_quality.h
@@ -7,20 +7,19 @@
 
   #define NSENSOR         2                 // No. sensors attached to ESP
   #define SERIALBAUD      9600              // Serial baudrate
-  #define DEF_INTER_TIME  20000000           // interrupt time in micro seconds
-  #define MIN_INTERRUPT   2                // Minimum interrupt time in seconds
+  #define DEF_INTER_TIME  20000000          // interrupt time in micro seconds
+  #define MIN_INTERRUPT   2                 // Minimum interrupt time in seconds
 
   #define NET_ACCESS      (String[][2]){{"ssid-wifi1", "passwordnet1"}, {"ssid-wifi2", "passwordnet2"}}
 
-  #define MQTTADDR        (const char *[]){"192.168.1.73"}    // MQTT broker IP address
-  #define MQTTUSERNAME    (char*)"myname"          // MQTT authentification username
-  #define MQTTPASSWORD    (char*)"mysecretpassword"       // MQTT authentification passord
+  #define MQTTADDR        (const char *[]){"192.168.1.73"}   // MQTT broker IP address
+  #define MQTTUSERNAME    (char*)"myname"                    // MQTT authentification username
+  #define MQTTPASSWORD    (char*)"mysecretpassword"          // MQTT authentification passord
   #define MQTT_PAYLOAD_S  512
 
-  #define SPIN            (int[]){39,35}                          // ESP32 Pin No.
-  
-  #define SNAME           (String[]){"NH3","CO"}         // SENSOR NAME/DESCRIPTION
-  #define MEASURE         (String[]){"/100ppmNH3","/100ppmCO"}     // SENSOR MEASURE UNITS
+  #define SPIN            (int[]){39,35}                       // ESP32 sensors Pin No.
+  #define SNAME           (String[]){"NH3","CO"}               // SENSOR NAME/DESCRIPTION
+  #define MEASURE         (String[]){"/100ppmNH3","/100ppmCO"} // SENSOR MEASURE UNITS
 
 
 #endif //esp32_air_quality
